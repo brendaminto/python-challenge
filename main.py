33f59595bcd38fe30a9920c0179d0f4e619e67e3
brendaminto <55676183+brendaminto@users.noreply.github.com>
2019-12-10 16:57:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,75 +1,56 @@
 import csv
 import os
 
-csvpath=os.path.join('/Users/brendaminto/Downloads','PyPoll','election_data.csv')
+csvpath=os.path.join('/Users/brendaminto','Pybank','budget_data.csv')
 
-total_votes = 0
-vote_khan = 0
-khan_per = 0.000
-vote_correy = 0
-correy_per = 0.000
-vote_li = 0
-li_per = 0.000
-vote_tooley = 0
-tooley_per = 0.000
-vote_dict = {}
+total_amount=0.00
+count = 0
+average_change = 0.00
+first_amount = 0.00
+last_amount = 0.00
+high_low = {}
+diff_amount = 0.00
 
-with open(csvpath,newline="") as csvfile:
+with open(csvpath, newline="") as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
-
-    if csv.Sniffer().has_header:
-        next(csvreader)
-for row in csvreader:
-        if row[2] == "Khan":
-          vote_khan +=1
     
-        if row[2] == "Correy":
-            vote_correy +=1
+    if csv.Sniffer().has_header:
+            next(csvreader)
 
-        if row[2] == "Li":
-             vote_li +=1
+    for row in csvreader:
+        if count == 0:
+            first_amount = float(row[1])
+            diff_amount = first_amount
+        total_amount += float(row[1]) 
+        last_amount = float(row[1]) 
+        if count > 0:
+            diff_amount = last_amount-diff_amount
+            high_low.update({row[0]:diff_amount})
+            diff_amount = last_amount
+        count += 1   
+        
 
-        if row[2] == "O'Tooley":
-             vote_tooley +=1
+    average_change = (last_amount-first_amount)/(count-1)
+    key, value = max(high_low.items(), key=lambda x:x[1])
+    key_1, value_1 = min(high_low.items(), key=lambda x:x[1])
 
-
-total_votes =  vote_khan + vote_correy + vote_li + vote_tooley
-khan_per = (vote_khan/total_votes)*100
-correy_per = (vote_correy/total_votes)*100
-li_per = (vote_li/total_votes)*100
-tooley_per = (vote_tooley/total_votes)*100
-
-vote_dict.update({"Khan":khan_per, "Correy":correy_per, "Li":li_per,"O'Tooley":tooley_per})
-
-winner_key,winner_value = max(vote_dict.items(), key=lambda x:x[1])
-
-# Write to file:
-
-file1 = open("py_poll.txt","w")
-file1.write("Election Results \n")
+file1 = open("py_bank.txt","w")
+file1.write("Financial Analysis \n")
 file1.write("---\n")
-file1.write("Total Votes: {0} \n".format(total_votes) )
-file1.write("---\n")
-file1.write("Khan: {0:.3f}% ({1}) \n".format(khan_per,vote_khan))
-file1.write("Correy: {0:.3f}% ({1}) \n".format(correy_per,vote_correy))
-file1.write("Li: {0:.3f}% ({1}) \n".format(li_per,vote_li))
-file1.write("O'Tooley: {0:.3f}% ({1}) \n".format(tooley_per,vote_tooley))
-file1.write("--- \n")
-file1.write("Winner: {} \n".format(winner_key))
-file1.write("---")
-file1.close
+file1.write("Total Months {0} \n".format(count))
+file1.write("Total : ${0} \n".format(total_amount))
+file1.write("Average Change: ${0:.2f} \n".format(average_change))
+file1.write("Greatest Increase in Profits: {0} (${1}) \n".format(key,value))
+file1.write("Greatest Decrease in Profits: {0} (${1}) \n".format(key_1,value_1))
+file1.close()
 
-#Print output to console:
+print("Financial Analysis")
+print("---")
+print("Total Months {0}".format(count))
+print("Total : ${0}".format(total_amount))  
+print("Average Change: ${0:.2f}".format(average_change))
+print("Greatest Increase in Profits: {0} (${1})".format(key,value))
+print("Greatest Decrease in Profits: {0} (${1})".format(key_1,value_1))
 
-print("Election Results")
-print("---")
-print("Total Votes: {0}".format(total_votes))
-print("---")
-print("Khan: {0:.3f}% ({1})".format(khan_per,vote_khan))
-print("Correy: {0:.3f}% ({1})".format(correy_per,vote_correy))
-print("Li: {0:.3f}% ({1})".format(li_per,vote_li))
-print("O'Tooley: {0:.3f}% ({1})".format(tooley_per,vote_tooley))
-print("---")
-print("Winner: {}".format(winner_key))
-print("---")
 
+          
